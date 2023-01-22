44f06479b7926224d22b0a3a700a5b412108dff6
Rashaad Akbar <rushda2000@gmail.com>
2023-01-22 14:39:05 +0530

    Initial commit

---
+++ food-website/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Food website</h1>
+      
+      what to put here
+    
+    </div>
+  )
+}
+
+export default App
